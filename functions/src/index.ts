9ddf539132daf265c5eb92286d44a0955d43b241
Eduardo Ferreira <eduardo.fernandes_ferreira@siemens-healthineers.com>
2021-03-31 19:18:17 +0200

    [#18] set taxon num media on media create/delete

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,45 @@
 import * as functions from 'firebase-functions';
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+// runs when media is created
+exports.onMediaCreate = functions.firestore
+  .document('media/{mediaUid}')
+  .onCreate((mediaSnap: functions.firestore.QueryDocumentSnapshot) => {
+    const taxonUid = mediaSnap.data().taxonUid;
+    // increase numMedia by 1
+    if (taxonUid) {
+      setNumMedia(taxonUid, 'increase');
+    }
+    return null;
+  });
+
+// runs when media is deleted
+exports.onMediaDelete = functions.firestore
+  .document('media/{mediaUid}')
+  .onDelete((mediaSnap: functions.firestore.QueryDocumentSnapshot) => {
+    const taxonUid = mediaSnap.data().taxonUid;
+    // decrease numMedia by 1
+    if (taxonUid) {
+      setNumMedia(taxonUid, 'decrease');
+    }
+    return null;
+  });
+
+function setNumMedia(taxonUid: string, direction: string): void {
+  // first, get the current numMedia
+  admin
+    .firestore()
+    .collection('genus')
+    .doc(taxonUid)
+    .get().then((taxon: functions.firestore.DocumentSnapshot) => {
+      const oldNumMedia = taxon.data()?.numMedia ? taxon.data()!.numMedia : 0;
+      const newNumMedia = direction === 'increase' ? oldNumMedia + 1 : oldNumMedia - 1;
+      // then set the new numMedia
+      admin
+        .firestore()
+        .collection('genus')
+        .doc(taxonUid)
+        .set({ numMedia: newNumMedia }, { merge: true });
+    });
+}
